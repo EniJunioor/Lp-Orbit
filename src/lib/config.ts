eacc23dc1841da82f045e1a0fc1e67d718b23451
Enivander Junior <105360680+EniJunioor@users.noreply.github.com>
2026-04-29 10:18:00 -0300

    feat: add system URL config and wire CTA buttons (Hero + BigCTA)

---
+++ src/lib/config.ts
@@ -0,0 +1,3 @@
+export const SYSTEM_URL = process.env.NEXT_PUBLIC_SYSTEM_URL ?? 'https://app.sistemaorbit.com.br';
+export const SYSTEM_LOGIN = `${SYSTEM_URL}/auth`;
+export const SALES_EMAIL = 'mailto:contato@sistemaorbit.com.br';
